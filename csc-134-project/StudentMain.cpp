0c277bac6351a378b66c0bc08f5a137723403ca5
JustinCB <JustinCB@opmbx.org>
2021-04-21 10:09:06 -0400

    Remove extraneous whitespace from documentation comment

--- csc-134-project/StudentMain.cpp
+++ csc-134-project/StudentMain.cpp
@@ -1,12 +1,12 @@
 ///////////////////////////////////////////////////////////////////////
-//                                                                     
+//
 // Filename: StudentMain.cpp
 // Date: April 21, 2021
 // Programmer: Justin Bester
 //
 // Description:
 //    Main file for the CSC-134 final project(student registration)
-//                                                                  
+//
 ///////////////////////////////////////////////////////////////////////
 #include <iostream>
 using namespace std;
