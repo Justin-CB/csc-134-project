658a9d7ddd8c533616c7f198c73bb1f452afaeaa
JustinCB <JustinCB@opmbx.org>
2021-04-21 09:37:56 -0400

    Create preliminary main implementation(subject to change based on ClassRoom implementation

--- csc-134-project/StudentMain.cpp
+++ csc-134-project/StudentMain.cpp
@@ -13,4 +13,12 @@ using namespace std;
 
 int main()
 {
+	/* This is preliminary, will need to be changed based on the actual function names */
+	ClassRoom classroom = new ClassRoom("students.txt");
+	classroom.sort_by_average();
+	classroom.display_students();
+	classroom.sort_by_lastname();
+	classroom.display_students();
+	classroom.display_average_grades();
+	classroom.display_num_students();
 }
