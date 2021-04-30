8f719581557b82aa1a53c52208bbddca6d37632f
JustinCB <JustinCB@opmbx.org>
2021-04-30 15:34:50 -0400

    Formatting(use tabs instead of spaces)

--- csc-134-project/ClassRoom.cpp
+++ csc-134-project/ClassRoom.cpp
@@ -8,128 +8,121 @@
 ClassRoom::ClassRoom()
 //default constructor.
 {
-    this->objectName = "CSC134";
-    this->numOfStudents = 0; /* You start with 0 students */
+	this->objectName = "CSC134";
+	this->numOfStudents = 0; /* You start with 0 students */
 
 }
 ClassRoom::ClassRoom(std::string objectName)
 {
-    this->objectName = objectName;
-    this->numOfStudents = 0; /* You start with 0 students */
-    //add DA here.
+	this->objectName = objectName;
+	this->numOfStudents = 0; /* You start with 0 students */
+	//add DA here.
 }
 ClassRoom::~ClassRoom()
 {
-    //delete the Dynamic Array in here.
+	//delete the Dynamic Array in here.
 
 }
 
 void ClassRoom::readFile(std::ifstream& inFile, int& cntr, Student*& arr) {
-    std::string firstName, lastName, SSN;
-    double eg1, eg2, eg3, eg4;
+	std::string firstName, lastName, SSN;
+	double eg1, eg2, eg3, eg4;
 
-    inFile.open("Students.txt");
+	inFile.open("Students.txt");
 
-    while (inFile.good()) {
-        while (inFile >> firstName >> lastName >> SSN >> eg1 >> eg2 >> eg3 >> eg4) {
-            cntr++;
+	while (inFile.good()) {
+		while (inFile >> firstName >> lastName >> SSN >> eg1 >> eg2 >> eg3 >> eg4) {
+			cntr++;
 
-            for (int j = 0; j < cntr; j++) {
-                arr[j].setFirst(firstName);
+			for (int j = 0; j < cntr; j++) {
+				arr[j].setFirst(firstName);
 
-                arr[j].setLast(lastName);
-                arr[j].setSSN(SSN);
-                double arrayGrades[4] = { eg1,eg2,eg3,eg4 };
-                arr[j].setGrades(arrayGrades);
-                arr[j].calAvg();
-
-
-            }
+				arr[j].setLast(lastName);
+				arr[j].setSSN(SSN);
+				double arrayGrades[4] = { eg1,eg2,eg3,eg4 };
+				arr[j].setGrades(arrayGrades);
+				arr[j].calAvg();
 
 
+			}
 
-        }
 
-    }
+
+		}
+
+	}
 
 }
 
+///////////////////////////////////////////////////////////////////////
+//
+// Function: spaces
+//
+// Description:
+//	Based on the number (1st parameter) it uses a for loop to place spaces.
+//	It's purpose is to avoid having to type out a bunch of spaces to
+//	line up the model.
+// Parameters:
+//	num  : an integer number that will dictate the number of spaces
+//
+// Returns:
+//	string : A string containing num spaces
+//
+///////////////////////////////////////////////////////////////////////
 std::string spaces(int num) {
-    std::string spacesHolder, rtnSpaces = "";
-    ///////////////////////////////////////////////////////////////////////
-    //
-    // Function: spaces                                        
-    //                                                                   
-    // Description:
-    //    based on the number (1st parameter) it uses a for loop to place spaces.
-    //		It's purpose is to avoid having to type out a bunch of spaces to 
-    //			line up the model and 
-    // Parameters:  
-    //    firstParam  : an integer number that will dictate the number of spaces          
-    //    secondParam : none             
-    //                                                       
-    // Returns:  
-    //    returnVal : a certian number of spaces                 
-    //                                            
-    ///////////////////////////////////////////////////////////////////////
+	std::string spaces = "";
 
-    for (int i = 0; i < num; i++) {
-        spacesHolder = spacesHolder + " ";
-    }
-    rtnSpaces = spacesHolder;
-    spacesHolder = "";
-    //"\t" = 8 spaces
-    return rtnSpaces;
+	for (int i = 0; i < num; i++) {
+		spaces += " ";
+	}
+	//"\t" = 8 spaces
+	return spaces;
 }
+///////////////////////////////////////////////////////////////////////
+//
+// Function: spaceDiff
+//
+// Description:
+//	gets the length of the previous string and determines how many spaces are needed
+//	 to be used with the spaces function.
+//
+// Parameters:
+//	numHolder  : the number of spaces in the string "item"
+//	item : string that the spaces should go after
+//
+// Returns:
+//	returnVal : the number of spaces needed for the function spaces
+//
+///////////////////////////////////////////////////////////////////////
 int spaceDiff(int numHolder, std::string item) {
-    ///////////////////////////////////////////////////////////////////////
-    //
-    // Function: spaceDiff                                          
-    //                                                                   
-    // Description:
-    //    gets the length of the previous string and determines how many spaces are needed
-    //     to be used with the spaces function.
-    //
-    // Parameters:  
-    //    firstParam  : the number of spaces in the string "item"          
-    //    secondParam : string that the spaces should go after    
-    //                                                       
-    // Returns:  
-    //    returnVal : the number of spaces needed for the function spaces                 
-    //                                            
-    ///////////////////////////////////////////////////////////////////////
-
-    std::string tempSpaceHoldler = "";
-    int cntr = 0;
-    for (int i = 0; i < (numHolder - item.length()); i++) {
-        cntr++;
-    }
-    return cntr;
+	/* ternary operator (condition) ? (return if true) : (return if false) *
+	 * They don't need to be in parentheses, but it's easier to understand */
+	return (item.length < numHolder) ? (numHolder - item.length) : (0);
 }
 double ClassRoom::overallAverage(Student*& arr, int studentsCntr) {
-    double totAverage = 0.0;
-    for (int i = 0; i < studentsCntr; i++) {
-        totAverage = totAverage + arr[i].getAvg();
-    }
-    totAverage = totAverage / studentsCntr;
-    return totAverage;
+	double totAverage = 0.0;
+	for (int i = 0; i < studentsCntr; i++) {
+		totAverage = totAverage + arr[i].getAvg();
+	}
+	totAverage = totAverage / studentsCntr;
+	return totAverage;
 }
 
 void ClassRoom::displaySummary(Student*& arr, int cntr) {
 
-    std::cout << "First Name \t Last Name \t SSN \t \t Exam 1 \t Exam 2 \t Exam 3 \t Exam 4 \t average" << std::endl;
-    for (int k = 0; k < cntr; k++) {
-        std::cout << arr[k].getFirst() << spaces(spaceDiff(17, arr[k].getFirst())) << arr[k].getLast() << spaces(spaceDiff(13, arr[k].getLast())) << arr[k].getSSN() << spaces(5) << arr[k].getGrades() << spaces(5) << arr[k].getAvg() << std::endl;
-    }
+	std::cout << "First Name \t Last Name \t SSN \t \t Exam 1 \t Exam 2 \t Exam 3 \t Exam 4 \t average" << std::endl;
+	for (int k = 0; k < cntr; k++) {
+		std::cout << arr[k].getFirst() << spaces(spaceDiff(17, arr[k].getFirst())) << arr[k].getLast() << spaces(spaceDiff(13, arr[k].getLast())) << arr[k].getSSN() << spaces(5) << arr[k].getGrades() << spaces(5) << arr[k].getAvg() << std::endl;
+	}
 
 
 }
 
 int ClassRoom::studentsCreatedNum(Student*& arr)
 {
-    int size;
-    size = _countof(arr);
+	int size;
+	size = _countof(arr);
 
-    return size;
+	return size;
 }
 
--- csc-134-project/ClassRoom.h
+++ csc-134-project/ClassRoom.h
@@ -1,38 +1,20 @@
-#pragma once
 #include <iostream>
 #include <string>
 #include "student.h"
 
 class ClassRoom {
+/*private:*/
 	std::string objectName;
 	int numOfStudents;
-	//dynamic array goes here.
-
+	Student * students;
 public:
 	ClassRoom();
-	
 	ClassRoom(std::string objectName);
-	
-	
 	~ClassRoom();
-
 	void readFile(std::ifstream& inFile, int& cntr, Student*& arr);
-
 	void sortByAverage();
-	// return array???
-	
-
 	void sortByLN();
-	
-
 	double overallAverage(Student*& arr, int studentsCntr);
-	
-
 	void displaySummary(Student*& arr, int counter);
-	
-
 	int studentsCreatedNum(Student*& arr);
-	
-
-
 };
